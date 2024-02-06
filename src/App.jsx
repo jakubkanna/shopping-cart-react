21f73eac9eea77049a95e9251db49929e8f6cf2a
Jakub Kanna <hello.jakubkanna@gmail.com>
2024-02-06 11:02:34 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header/header";
+import Main from "./components/main/main";
+import Footer from "./components/footer/footer";
+
+export default function App() {
+  return (
+    <>
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </>
+  );
+}
